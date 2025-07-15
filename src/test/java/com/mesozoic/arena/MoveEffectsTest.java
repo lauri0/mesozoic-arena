b77826e922b1c02dc5f467dfa1ef56a5a5f7fccb
lauri0 <lauri.kongas@hotmail.com>
2025-07-15 21:29:11 +0300

    Add multi-hit and buff move effects

--- src/test/java/com/mesozoic/arena/MoveEffectsTest.java
+++ src/test/java/com/mesozoic/arena/MoveEffectsTest.java
@@ -49,4 +49,61 @@ public class MoveEffectsTest {
         battle.executeRound(heal, noop);
         assertEquals(80, active.getHealth());
     }
+
+    @Test
+    public void testDoubleAttackHitsTwice() {
+        Move doubleHit = new Move("Double", 1, 0, 0, List.of(new Effect("double attack")));
+        Move noop = new Move("Wait", 0, 0, 0, List.of());
+        Dinosaur attacker = new Dinosaur("Attacker", 100, 50, "assets/animals/allosaurus.png", 100, 10, List.of(doubleHit), null);
+        Dinosaur defender = new Dinosaur("Defender", 100, 50, "assets/animals/allosaurus.png", 100, 10, List.of(noop), null);
+        Player p1 = new Player(List.of(attacker));
+        Player p2 = new Player(List.of(defender));
+        Battle battle = new Battle(p1, p2);
+
+        battle.executeRound(doubleHit, noop);
+        assertEquals(80, defender.getHealth());
+    }
+
+    @Test
+    public void testTripleAttackHitsThrice() {
+        Move tripleHit = new Move("Triple", 1, 0, 0, List.of(new Effect("triple attack")));
+        Move noop = new Move("Wait", 0, 0, 0, List.of());
+        Dinosaur attacker = new Dinosaur("Attacker", 100, 50, "assets/animals/allosaurus.png", 100, 10, List.of(tripleHit), null);
+        Dinosaur defender = new Dinosaur("Defender", 100, 50, "assets/animals/allosaurus.png", 100, 10, List.of(noop), null);
+        Player p1 = new Player(List.of(attacker));
+        Player p2 = new Player(List.of(defender));
+        Battle battle = new Battle(p1, p2);
+
+        battle.executeRound(tripleHit, noop);
+        assertEquals(70, defender.getHealth());
+    }
+
+    @Test
+    public void testFrenzyRaisesAttack() {
+        Move frenzy = new Move("Rage", 0, 0, 0, List.of(new Effect("frenzy")));
+        Move noop = new Move("Wait", 0, 0, 0, List.of());
+        Dinosaur attacker = new Dinosaur("Attacker", 100, 50, "assets/animals/allosaurus.png", 100, 10, List.of(frenzy), null);
+        Dinosaur defender = new Dinosaur("Defender", 100, 50, "assets/animals/allosaurus.png", 100, 10, List.of(noop), null);
+        Player p1 = new Player(List.of(attacker));
+        Player p2 = new Player(List.of(defender));
+        Battle battle = new Battle(p1, p2);
+
+        battle.executeRound(frenzy, noop);
+        assertEquals(2, attacker.getAttackStage());
+    }
+
+    @Test
+    public void testAdrenalineRaisesAttackAndSpeed() {
+        Move adrenaline = new Move("Boost", 0, 0, 0, List.of(new Effect("adrenaline")));
+        Move noop = new Move("Wait", 0, 0, 0, List.of());
+        Dinosaur attacker = new Dinosaur("Attacker", 100, 50, "assets/animals/allosaurus.png", 100, 10, List.of(adrenaline), null);
+        Dinosaur defender = new Dinosaur("Defender", 100, 50, "assets/animals/allosaurus.png", 100, 10, List.of(noop), null);
+        Player p1 = new Player(List.of(attacker));
+        Player p2 = new Player(List.of(defender));
+        Battle battle = new Battle(p1, p2);
+
+        battle.executeRound(adrenaline, noop);
+        assertEquals(1, attacker.getAttackStage());
+        assertEquals(1, attacker.getSpeedStage());
+    }
 }
